569502602cb111662009946eff5fae32ff4db4e0
Grishma Singh <117310733+grishmasingh@users.noreply.github.com>
2022-11-14 01:23:26 +0000

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
+
 import React from "react";
 
 const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-screen bg-gradient-to-b from-white to-white text-black"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -12,26 +13,27 @@ const About = () => {
             About
           </p>
         </div>
-
-        <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+        <p className="text-xl mt-10">
+          Hello Everyone and Thank you for taking the time to visit my Website! <br/>
+          A little bit about me, myself & I .....
+    
         </p>
 
         <br />
 
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
-        </p>
+        <p className="text">
+          I'm Grishma and I am currently working as a Software Developer for a bank based in London, UK and I mainly use JavaScript and Python in my current profession. I also have another weapon of choice which is Cyber Security which I studied in University where I applied theoretical and practical exercises of Enterprise Security and Cyber Governance, both which plays a huge role in safeguarding our infrastructures.
+           <br/><br/>Building web applications using JavaScript, React.js, HTML & Tailwind CSS is what I really enjoy doing as I love to see results displayed aesthetically. I have recently also discovered an admiration for Python and have built serveral mini projects which are in my Portfolio section of this website. <br/> 
+          
+          
+          <br/>Tenacity and  Curiousity are traits I aim to constanty develop when I am programming as Coding is a lifelong learning path and there is always a way to constantly optimize your programs. <br/> Furthermore, a collabarative and social environment where individuals are able to bounce off each other's ideas is where I enjoy working alongside the most:  As they say - Team Work makes the Dream Work!
+
+
+    <br/><br/>
+    Outside of Coding, I love to hit the Gym daily and play my guitar as a way of relieving stress after a long session of debugging a single syntax error....which probably explains my sudden interest in Python.
+    Oh yeah, Did i mention I also have an unhealthy obsession for Cookies and Boba Tea ? 
+      
+           </p> 
       </div>
     </div>
   );
